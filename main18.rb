a3e375b5c5ad8fa4d37dd2ff028bd6aa6c7e2d27
Hossein <hosseinsalmani1373@gmail.com>
2021-06-05 22:03:05 +0430

    if

---
+++ main18.rb
@@ -0,0 +1,19 @@
+# شرط if
+
+x = 20
+if x == 20
+  puts "Yes"
+end
+
+x = 20
+if x == 12
+  puts "Yes"
+else
+  puts "No"
+end
+
+name = "ALI"
+new_name = "ali"
+if new_name.downcase == name.downcase
+  puts "Same Name!"
+end
